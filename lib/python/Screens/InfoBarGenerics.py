c4a78b8618fc53b69d6ba086d356bfc758c0321a
xtrendmaster <amicimax@aol.com>
2013-07-17 16:30:54 +0100

    green button mapped to pluginbrowser

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2165,6 +2165,7 @@ class InfoBarSubserviceSelection:
 	def __init__(self):
 		self["SubserviceSelectionAction"] = HelpableActionMap(self, "InfobarSubserviceSelectionActions",
 			{
+				"GreenPressed": (self.GreenPressed),
 				"subserviceSelection": (self.subserviceSelection, _("Subservice list...")),
 			})
 
@@ -2182,6 +2183,18 @@ class InfoBarSubserviceSelection:
 		self.onClose.append(self.__removeNotifications)
 
 		self.bsel = None
+
+	def GreenPressed(self):
+		service = self.session.nav.getCurrentService()
+		subservices = service and service.subServices()
+		if not subservices or subservices.getNumberOfSubservices() == 0:
+			try:
+				from Screens.PluginBrowser import PluginBrowser
+				self.session.open(PluginBrowser)
+			except:
+				pass
+		else:
+			self.subserviceSelection()
 
 	def __removeNotifications(self):
 		self.session.nav.event.remove(self.checkSubservicesAvail)
